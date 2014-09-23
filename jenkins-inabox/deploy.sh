590eb8b50eb344d3ab0a063e840f25e946570b75
Terry Howe <terrylhowe@gmail.com>
2014-09-23 16:02:44 -0600

    remove deploy.sh

--- jenkins-inabox/deploy.sh
+++
@@ -1,9 +0,0 @@
-stackato target https://api.${DOMAIN}
-stackato login terryhowe
-stackato unmap inabox inabox.${DOMAIN} || true
-stackato stop inabox
-stackato push -n
-echo "Deploying http://inabox.${DOMAIN}"
-stackato start inabox
-stackato map inabox inabox.${DOMAIN}
-
