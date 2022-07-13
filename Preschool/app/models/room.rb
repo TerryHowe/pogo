231280ccf6be883df3a2e935ca948fccf6eea973
Terry Howe <tlhowe@amazon.com>
2022-07-13 07:01:11 -0600

    remove junk

--- Preschool/app/models/room.rb
+++
@@ -1,3 +0,0 @@
-class Room < ActiveRecord::Base
-  validates :name, :presence => true
-end
--- Preschool/app/models/teacher_observer.rb
+++
@@ -1,6 +0,0 @@
-class TeacherObserver < ActiveRecord::Observer
-  def before_save(teacher)
-    return false if teacher.room == nil
-    return false if teacher.photo == nil
-  end
-end
